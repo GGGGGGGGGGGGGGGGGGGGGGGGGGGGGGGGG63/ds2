4ddf70a8b6e215bc290720c1d3265e6147c3dec0
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <1098035852@qq.com>
2019-12-13 19:00:44 +0800

    k
    k

--- 456.cpp
+++ 456.cpp
@@ -1,64 +1,68 @@
-#include <stdio.h>
-
-#define MAXTABLESIZE 100000  /* 允许开辟的最大散列表长度 */
-typedef int ElementType;     /* 关键词类型用整型 */
-typedef int Index;           /* 散列地址类型 */
-typedef Index Position;      /* 数据所在位置与散列地址是同一类型 */
-/* 散列单元状态类型，分别对应：有合法元素、空单元、有已删除元素 */
-typedef enum { Legitimate, Empty, Deleted } EntryType;
-
-typedef struct HashEntry Cell; /* 散列表单元类型 */
-struct HashEntry{
-    ElementType Data; /* 存放元素 */
-    EntryType Info;   /* 单元状态 */
-};
-
-typedef struct TblNode *HashTable; /* 散列表类型 */
-struct TblNode {   /* 散列表结点定义 */
-    int TableSize; /* 表的最大长度 */
-    Cell *Cells;   /* 存放散列单元数据的数组 */
-};
-
-HashTable BuildTable(); /* 裁判实现，细节不表 */
-Position Hash( ElementType Key, int TableSize )
-{
-    return (Key % TableSize);
-}
-
-#define ERROR -1
-Position Find( HashTable H, ElementType Key );
-
-int main()
-{
-    HashTable H;
-    ElementType Key;
-    Position P;
-
-    H = BuildTable(); 
-    scanf("%d", &Key);
-    P = Find(H, Key);
-    if (P==ERROR)
-        printf("ERROR: %d is not found and the table is full.\n", Key);
-    else if (H->Cells[P].Info == Legitimate)
-        printf("%d is at position %d.\n", Key, P);
-    else
-        printf("%d is not found.  Position %d is returned.\n", Key, P);
-
-    return 0;
-}
-
-/* 你的代码将被嵌在这里 */
-Position Find( HashTable H, ElementType Key ){
-	Position p0,p;
-	int Cnum=0;//冲突数
-	p=p0=Hash(Key,H->TableSize);
-	while(H->Cells[p].Info!=Empty&&H->Cells[p].Data!=Key){
-		Cnum++;
-		if(Cnum==MAXTABLESIZE){
-			return ERROR;
-		}
-		p=(p0+Cnum)%H->TableSize;		
-	}
-	return p;
-}
-
+ #include<iostream>
+  #include<cmath>
+  using namespace std;
+  
+  class Complex
+  {
+  private:
+      double a, b;
+  public:
+     Complex()
+     {
+         a = 0, b = 0;
+     }
+     Complex(double A, double B)
+     {
+         a = A, b = B;
+     }
+     void set(double A, double B)
+    {
+         a = A, b = B;
+     }
+     void display()
+     {
+         cout << a << " " << b << endl;
+    }
+    friend double operator - (Complex C1, Complex C2);
+ };
+ 
+ double operator -(Complex C1, Complex C2)
+ {
+     return sqrt(pow(C1.a - C2.a, 2.0) + pow(C1.b - C2.b, 2.0));
+ }
+ 
+ template<class T>
+ double dist(T a, T b)
+ {
+     return abs(a - b);
+ }
+ 
+ int main()
+ {
+     int flag;
+ 
+     while(cin >> flag, flag != 0)
+     {
+        if(flag == 1)
+         {
+             int a, b;
+             cin >> a >> b;
+             cout << dist(a, b) << endl;
+         }
+         else if(flag == 2)
+         {
+             float a, b;
+             cin >> a >> b;
+             cout << dist(a, b) << endl;
+         }
+         else if(flag == 3)
+         {             Complex a, b;
+             double a1, a2, b1, b2;
+             cin >> a1 >> a2 >> b1 >> b2;
+             a.set(a1, a2), b.set(b1, b2);
+             cout << dist(a, b) << endl;
+         }
+     }
+ 
+     return 0;
+ }
