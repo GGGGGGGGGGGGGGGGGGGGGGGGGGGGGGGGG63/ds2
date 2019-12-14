fb0b92b9c1e9c1453d9268e18c893a82da38a83b
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <1098035852@qq.com>
2019-12-14 21:05:02 +0800

    n
    n

--- gyttyd.cpp
+++ gyttyd.cpp
@@ -1,225 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAXN 105
+int main(void)
+{
+	int i,n,j,sum=0,z=0;
+	scanf("%d",&n);
+	char str[105][105];
+	int b[18]={7,9,10,5,8,4,2,1,6,3,7,9,1,0,5,8,4,2};
+	char m[]={'1','0','X','9','8','7','6','5','4','3','2'};
+	for(i=0;i<n;i++)
+	{
+		scanf("%s",str[i]);//str[320124198808240056	
+	}
+    for(i=0;i<n;i++)
+    {
+    	for(j=0;j<17;j++)
+    	{
+    		str[i][j]-='0';
+    		printf("%d",str[i][j]);
+    		str[i][j]=str[i][j]*b[j];
+    		sum=sum+str[i][j];	
+		}
+		z=sum%11;//6
+		printf("%d\n",z);
+		//if(z==0)m='1';if(z==1)m='0';if(z==2)m='X';if(z==3)m='9';if(z==4)m='8';if(z==5)m='7';if(z==6)m='6';if(z==7)m='5';if(z==8)m='4';if(z==9)m='3';if(z==10)m='2';
+		printf("%c,%d",m,sizeof(m));
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-struct stackstruct                     /*栈的结构体*/
-{
-	int id;
-	int time;
-	struct stackstruct *pre;
-	struct stackstruct *next;
-};
- 
-struct queuestruct
-{
-	int id;
-	struct queuestruct *next;
-};
- 
-struct stackstruct *stackhead1, *stackend1;
-struct stackstruct *stackhead2, *stackend2;
-struct queuestruct *queuehead, *queueend;
-int stack1count, stack2count;                         /*栈中元素总数*/
-int queuecount;                         /*队列中元素总数*/
- 
-void push(int flag, struct stackstruct *p)
-{
-	struct stackstruct *stack;
-	if (flag == 0)                  /*栈1进栈操作*/
-	{
-		if (stack1count == 0)
-		{
-			stackhead1 = (struct stackstruct *)malloc(sizeof(struct stackstruct));
-			stackhead1->id = p->id;
-			stackhead1->time = p->time;
-			stackhead1->next = NULL;
-			stackhead1->pre = NULL;
-			stackend1 = stackhead1;
 		}
-		else
-		{
-			stack = (struct stackstruct *)malloc(sizeof(struct stackstruct));
-			stack->id = p->id;
-			stack->time = p->time;
-			stackend1->next = stack;
-			stack->pre = stackend1;
-			stack->next = NULL;
-			stackend1 = stack;
-		}
-		stack1count++;
-	}
-	else if (flag == 1)               /*栈2进栈操作*/
-	{
-		if (stack2count == 0)
-		{
-			stackhead2 = (struct stackstruct *)malloc(sizeof(struct stackstruct));
-			stackhead2->id = p->id;
-			stackhead2->time = p->time;
-			stackhead2->next = NULL;
-			stackhead2->pre = NULL;
-			stackend2 = stackhead2;
-		}
-		else
-		{
-			stack = (struct stackstruct *)malloc(sizeof(struct stackstruct));
-			stack->id = p->id;
-			stack->time = p->time;
-			stackend2->next = stack;
-			stack->pre = stackend2;
-			stack->next = NULL;
-			stackend2 = stack;
-		}
-		stack2count++;
-	}
-}
- 
-struct stackstruct *pop(int id, int time)
-{
-	struct stackstruct *stack;
-	stack = (struct stackstruct *)malloc(sizeof(struct stackstruct));
- 
-	if (stackend1->id != id)
-	{
-		stack->id = stackend1->id;
-		stack->time = stackend1->time;
-		stack->pre = stackend1->pre;
-		free(stackend1);
-		stackend1 = stack->pre;
-		stackend1->next = NULL;
-		stack1count--;
-	}
-	else
-	{
-		stack->id = stackend1->id;
-		stack->time = stackend1->time;
-		stack->pre = stackend1->pre;
-		printf("%d号汽车出停车场\n", id);
-		printf("停车场停留时间: %d\n", time - stack->time);
-		printf("应该缴纳的费用(单价: 5): %d\n", 5 * (time - stack->time));
-		printf("谢谢回顾，祝您旅途愉快！\n"); 
-		printf("--------------------------------------------------------------------------\n");
-		free(stackend1);
-		if (--stack1count == 0)
-			stackend1 = stackhead1 = NULL;
-		else
-		{
-			stackend1 = stack->pre;
-			stackend1->next = NULL;
-		}
-		stack = NULL;
-	}
-	return stack;
-}
- 
-struct stackstruct *pop1()
-{
-	struct stackstruct *stack;
-	stack = (struct stackstruct *)malloc(sizeof(struct stackstruct));
- 
-	stack->id = stackend2->id;
-	stack->time = stackend2->time;
-	stack->pre = stackend2->pre;
-	free(stackend2);
-	stackend2 = stack->pre;
-	stack2count--;
- 
-	return stack;
-}
- 
-void Enqueue(struct stackstruct *p)
-{
-	struct queuestruct *queue;
-	if (queuecount == 0)
-	{
-		queuehead = (struct queuestruct *)malloc(sizeof(struct queuestruct));
-		queuehead->id = p->id;
-		queuehead->next = NULL;
-		queueend = queuehead;
-	}
-	else
-	{
-		queue = (struct queuestruct *)malloc(sizeof(struct queuestruct));
-		queue->id = p->id;
-		queue->next = NULL;
-		queueend->next = queue;
-		queueend = queue;
-	}
-	queuecount++;
-}
- 
-struct stackstruct *Dequeue(int time)
-{
-	struct stackstruct *stack;
-	stack = (struct stackstruct *)malloc(sizeof(struct stackstruct));
- 
-	stack->id = queuehead->id;
-	stack->time = time;
-	if (--queuecount == 0)
-	{
-		queuehead = NULL;
-		queueend = NULL;
-	}
-	else
-		queuehead = queuehead->next;
-	return stack;
-}
-int main()
-{
-	int n;
-	char s;
-	struct stackstruct *p;
- 
-	printf("请输入停车场的狭长通道可停放汽车数量：");
-	scanf("%d", &n);
-	getchar();
-	stack1count = stack2count = queuecount = 0;
-	p = (struct stackstruct *)malloc(sizeof(struct stackstruct));
- 
-	while (scanf("(%c,%d,%d)", &s, &p->id, &p->time) != EOF)
-	{
-		getchar();
-		if (s == 'E')
-			break;
-		else if (s == 'A')                /*汽车到达*/
-		{
-			if (stack1count < n)                 /*栈未满，进栈操作*/
-			{
-				push(0, p);
-				printf("%d号汽车进入停车场\n", stackend1->id);
-				printf("进入停车场时间: %d\n", stackend1->time);
-				printf("停车位置: %d\n", stack1count);
-					printf("--------------------------------------------------------------------------\n");
-			}
-			else                                /*栈满，进队列操作*/
-			{
-				Enqueue(p);
-				printf("%d号汽车进入便道\n", p->id);
-				printf("进入便道时间: %d\n", p->time);
-				printf("便道位置: %d\n", queuecount);
-					printf("--------------------------------------------------------------------------\n");
-			}
-		}
-		else if (s == 'D')                /*汽车离去*/
-		{
-			struct stackstruct *temp;
-			while ((temp = pop(p->id, p->time)) != NULL)
-			{
-				push(1, temp);                  
-			}
-			while (stack2count != 0)
-			{
-				push(0, pop1());
-			}
-			if (queuecount != 0)
-			{
-				push(0, Dequeue(p->time));
-				printf("%d号汽车进入停车场\n", stackend1->id);
-				printf("进入时间: %d\n", stackend1->time);
-				printf("停车位置: %d\n", stack1count);
-					printf("--------------------------------------------------------------------------\n");
-			}
-		}
-	}
-	return 0;}
-
+	return 0;
+} 
