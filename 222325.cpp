28486254c8b3a4acfc32df88b3a0ed08abd18275
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <1098035852@qq.com>
2019-12-20 23:01:46 +0800

    444
    444

--- 222325.cpp
+++ 222325.cpp
@@ -1,49 +1,44 @@
-
-#include<iostream>
-using namespace std; 
-class Horse{
-    public:
-      Horse(){
-          cout<<"Horse 申请了空间..."<<endl;
-      }
-      virtual void Fly(){
-          cout<<"Just a horse."<<endl;
-      }
-      virtual ~Horse(){
-          cout<<"Horse 释放了空间..."<<endl;
-      }
-};
-class Pegasus:public Horse{
-    public:
-      Pegasus(){
-          cout<<"Pegasus 申请了空间..."<<endl;
-      }
-      void Fly(){
-          cout<<"I can fly!"<<endl;
-      }
-      /*~Pegasus(){
-          cout<<"Pegasus 释放了空间..."<<endl;
-      }*/ 
-};
+#include <iostream>
 using namespace std;
+enum CPU_Rank{P1=1,P2,P3,P4,P5,P6,P7};
+int cnt=0;
+class CPU{
+	public: 
+	CPU(CPU_Rank a=P1,int b=2,int c=100);
+	CPU(const CPU &cx){
+		cout<<"copy create a CPU!"<<endl;
+		f=cx.f;
+		v=cx.v;
+		r=cx.r;
+		cnt++;
+	}
+	void showinfo();
+	
+	private:
+		int f,v;
+		CPU_Rank r;	
+};
+CPU::CPU(CPU_Rank a,int b,int c):r(a),f(b),v(c){cnt++;cout<<"create a CPU!"<<endl;}
+void CPU::showinfo(){
+	cout<<"rank:"<<r<<endl<<"frequency:"<<f<<endl<<"voltage:"<<v<<endl;
+	if(cnt==3){
+		for(int i=1;i<=cnt;i++){
+			cout<<"destruct a CPU!"<<endl;
+		}
+	}
+}
 int main()
 {
-	Horse *p1 = new Horse; //输出：Horse 申请了空间...
-	Horse *p2 = new Pegasus; /*  输出两行：  
-	                             Horse 申请了空间...
-	                             Pegasus 申请了空间...   
-			         */	
-	cout << endl; 
+	CPU a(P6,3,300); 
 	
-	p1->Fly(); //输出：Just a horse.
-	p2->Fly(); //输出：I can fly!
-	cout << endl; 
-	
-	delete p1; //输出：Horse 释放了空间...
-	delete p2;  /* 输出两行：
-	               Pegasus 释放了空间... 
-	               Horse 释放了空间... 
-		    */
-	return 0;
-}
+	cout<<"cpu a's parameter"<<endl;
+	a.showinfo(); //显示性能参数
 
+	CPU b; 
+	cout<<"cpu b's parameter"<<endl;
+	b.showinfo(); //显示性能参数
+
+	CPU c(a); 
+	cout<<"cpu c's parameter"<<endl;
+	c.showinfo(); //显示性能参数
+}
