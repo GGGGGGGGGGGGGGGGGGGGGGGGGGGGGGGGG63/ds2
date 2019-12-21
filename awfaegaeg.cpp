1bb7998c6f2ffd39f8a7a2ccfdef3d85178c8406
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <1098035852@qq.com>
2019-12-21 23:18:42 +0800

    666
    666

--- awfaegaeg.cpp
+++ awfaegaeg.cpp
@@ -1,39 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef char ElementType;
-typedef struct TNode *Position;
-typedef Position BinTree;
-struct TNode{
-    ElementType Data;
-    BinTree Left;
-    BinTree Right;
-};
-
-//BinTree CreatBinTree(); /* 实现细节忽略 */
-int GetHeight( BinTree BT );
-
-int main()
-{
-    BinTree BT = CreatBinTree();
-    printf("%d\n", GetHeight(BT));
-    return 0;
-}
-int GetHeight( BinTree BT )
-{
-	int HL ,HR ,MAXH;
-	if(BT)
-	{
-		HL=GetHeight(BT->Left);
-	    HR=GetHeight(BT->Right);
-	    MAXH=HL>HR?HL:HR;
-	    return (MAXH+1);
-	}
-	else
-	{
-		return 0;
-	}
-	
-
-} 
-/* 你的代码将被嵌在这里 */
+ #include<iostream>
+  #include<cmath>
+  using namespace std;
+  
+  class Complex
+  {
+  private:
+      double a, b;
+  public:
+     Complex()
+     {
+         a = 0, b = 0;
+     }
+     Complex(double A, double B)
+     {
+         a = A, b = B;
+     }
+     void set(double A, double B)
+    {
+         a = A, b = B;
+     }
+     void display()
+     {
+         cout << a << " " << b << endl;
+    }
+    friend double operator - (Complex C1, Complex C2);
+ };
+ 
+ double operator -(Complex C1, Complex C2)
+ {
+     return sqrt(pow(C1.a - C2.a, 2.0) + pow(C1.b - C2.b, 2.0));
+ }
+ 
+ template<class T>
+ double dist(T a, T b)
+ {
+     return abs(a - b);
+ }
+ 
+ int main()
+ {
+     int flag;
+ 
+     while(cin >> flag, flag != 0)
+     {
+        if(flag == 1)
+         {
+             int a, b;
+             cin >> a >> b;
+             cout << dist(a, b) << endl;
+         }
+         else if(flag == 2)
+         {
+             float a, b;
+             cin >> a >> b;
+             cout << dist(a, b) << endl;
+         }
+         else if(flag == 3)
+         {             Complex a, b;
+             double a1, a2, b1, b2;
+             cin >> a1 >> a2 >> b1 >> b2;
+             a.set(a1, a2), b.set(b1, b2);
+             cout << dist(a, b) << endl;
+         }
+     }
+ 
+     return 0;
+ }
