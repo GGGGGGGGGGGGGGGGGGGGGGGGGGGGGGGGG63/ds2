95942512a8c6f7fcf357278f596cd877aab1d80f
GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG63 <1098035852@qq.com>
2019-12-23 21:27:38 +0800

    ab
    ab

--- srgr.cpp
+++ srgr.cpp
@@ -1,55 +1,37 @@
-#include <stdio.h>
 
-#define N 1000
-int arr[N];
-
-/* 对长度为n的数组arr执行选择排序 */
-void selectionSort(int arr[], int n);
-
-/* 打印长度为n的数组arr */
-void printArray(int arr[], int n);
-
-void swap(int *xp, int *yp) {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-
+#include <bits/stdc++.h>
+using namespace std;
 int main() {
-    int n, i;
-    scanf("%d", &n);
-    for (i = 0; i < n; ++i) {
-        scanf("%d", &arr[i]);
+    vector<int>v;
+    //freopen("out.txt","w",stdout);
+    stack<int> s;
+    int n,t;
+    cin>>n;
+    string str;
+    vector<int>::iterator it;
+    while(n--) {
+        cin>>str;
+        if(str=="Push") {
+            cin>>t;
+            s.push(t);
+            vector<int>::iterator tt = lower_bound(v.begin(),v.end(),t);
+            v.insert(tt,t);
+        } else if(str=="Pop") {
+            if(s.empty())
+                cout<<"Invalid"<<endl;
+            else {
+                cout<<s.top()<<endl;
+                vector<int>::iterator i = lower_bound(v.begin(),v.end(),s.top());
+                v.erase(i);
+                s.pop();
+            }
+        } else {
+            if(s.empty())
+                cout<<"Invalid"<<endl;
+            else {
+                cout<<v[(v.size()+1)/2-1]<<endl;
+            }
+        }
     }
-    selectionSort(arr, n);
-    printArray(arr, n);
     return 0;
-}
-/* 打印长度为n的数组arr */
-void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("%d", arr[i]);
-        if (i < n - 1)  /* 下标0..n-2每个元素后面有个空格 */
-            printf(" ");/*下标n-1,也就是最后一个元素后面没有空格*/
-    }
-    printf("\n");/* 一行打印完后换行 */
-}
-
-void selectionSort(int arr[], int n)
-{
-	int i,j,min;
-	for(i=0;i<n-1;i++)
-	{
-		min=i;
-		for(j=i+1;j<n;j++)
-		{
-			if(arr[j]<arr[min])
-			{
-				min=j;
-			}
-		}
-	swap(&arr[i],&arr[min]);
-	}
- } 
-/* 你的代码将嵌在这里 */
+} 
